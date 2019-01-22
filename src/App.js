d5023ead4169ca8535887567ac6d14d9291f3ce5
Douglas Dias <douglasmarquesdias@hotmail.com>
2019-01-22 00:09:01 -0200

    'consumindo-rest'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PlacarContainer from './components/PlacarContainer'
-
-const url = 'http://127.0.0.1:8080/dados';
 
 const dados = {
   partida: {
@@ -19,14 +16,24 @@ const dados = {
   }
 };
 
-var request = new XMLHttpRequest();
-request.open('GET', url, true);
-
-request.onload = function () {   
-  console.log(request); 
-}
-
 export default class App extends React.Component {
+
+    componentDidMount() {
+        this.dados()
+          .then(res => this.setState({ response: res.express }))
+          .catch(err => console.log(err));
+      }
+    
+      dados = async () => {
+        const response = await fetch('http://127.0.0.1:8080/dados');
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        console.log(JSON.stringify(body));
+        return body;
+      };
+
+      
+
   render(){
       return <PlacarContainer {...dados} tempo={90} />           
   }
