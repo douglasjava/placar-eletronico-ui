e61cc943d181ec7f82532c61fee4f2329a2687e1
Douglas Dias <douglasmarquesdias@hotmail.com>
2019-01-21 23:26:04 -0200

    'criando-projeto'

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PlacarContainer from './components/PlacarContainer'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const url = 'http://127.0.0.1:8080/dados';
+
+const dados = {
+  partida: {
+      estadio: "Mineirão/MG",
+      data: "01/06/2016",
+      horario: "19hs",
+  },
+  casa: {
+      nome: "Cruzeiro",
+  },
+  visitante:{
+      nome: "Flamengo",
   }
+};
+
+var request = new XMLHttpRequest();
+request.open('GET', url, true);
+
+request.onload = function () {   
+  console.log(request); 
 }
 
-export default App;
+export default class App extends React.Component {
+  render(){
+      return <PlacarContainer {...dados} tempo={90} />           
+  }
+}
---
+++ src/components/Time.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import BotaoGol from './BotaoGol';
+
+export default class Time extends React.Component {
+    render() {
+        return (
+            <div>
+               <h1>{this.props.nome}</h1>
+               <h1>{this.props.gols}</h1>
+               <BotaoGol marcarGol={this.props.marcarGol} />
+            </div>
+        );
+    }
+}
